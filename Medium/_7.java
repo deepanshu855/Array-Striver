d4ed119f78386e2fc67dfee1a63680d004ff10a8
Deepanshu Sharma <deepanshusharma040@gmail.com>
2026-05-13 18:06:27 +0530

    Alternate Number

--- Medium/_7.java
+++ Medium/_7.java
@@ -8,6 +8,8 @@ package Medium;
 // 3. The rearranged array begins with a positive integer.
 // 4. Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
 
+import java.util.*;
+
 public class _7 {
     public static int[] rearrangeArray(int[] nums) {
         int[] result= new int[nums.length];
@@ -26,8 +28,49 @@ public class _7 {
     }
 
     // Alternate Number- another version of this question.- Code need to be done
+    public static int[] alternateNumber(int[] nums){
+        List<Integer> positives= new ArrayList<>();
+        List<Integer> negatives= new ArrayList<>();
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>0) positives.add(nums[i]);
+            else negatives.add(nums[i]);
+        }
+
+        int[] answer= new int[nums.length];
+
+        if(positives.size()>negatives.size()){
+            int idx=0;
+            for(int i=idx; i<negatives.size(); i++){
+                answer[2*idx]= positives.get(i);
+                answer[2*idx+1]= negatives.get(i);
+                idx++;
+            }
+
+            int k=2*idx;
+            for(int i=idx; i<positives.size(); i++){
+                answer[k++]= positives.get(i);
+            }
+        } else {
+            int idx=0;
+            for(int i=idx; i<positives.size(); i++){
+                answer[2*idx]= positives.get(i);
+                answer[2*idx+1]= negatives.get(i);
+                idx++;
+            }
+
+            int k=2*idx;
+            for(int i=idx; i<negatives.size(); i++){
+                answer[k++]= negatives.get(i);
+            }
+        }
+
+        return answer;
+    }
 
     static void main(String[] args) {
         int[] ans= rearrangeArray(new int[]{3,1,-2,-5,2,-4});
+        int[] result= alternateNumber(new int[]{1,2,-4,-5,3,60});
+        System.out.println(Arrays.toString(result));
     }
 }
